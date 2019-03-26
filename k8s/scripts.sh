9ab9de7c73b8b33ab1141d6f3ac0e281ed8b6acc
calvin <calvin@localhost.localdomain>
2019-03-26 05:32:48 -0400

    添加kubernetes的官方源,从而安装最新版本: centos提供的源版本很老

---
+++ k8s/scripts.sh
@@ -0,0 +1,5 @@
+wget https://raw.githubusercontent.com/johannescalvin/centos-config/master/k8s/kubernetes.repo -O /etc/yum.repos.d/kubernetes.repo
+
+yum -y install kubelet kubeadm kubectl
+
+
--- script.sh
+++ script.sh
@@ -2,4 +2,5 @@
 wget https://raw.githubusercontent.com/johannescalvin/centos-config/master/chrome/google-chrome.repo -O /etc/yum.repos.d/google-chrome.repo
 yum install -y  google-chrome-stable --nogpgcheck
 
+sh k8s/scripts.sh
 
